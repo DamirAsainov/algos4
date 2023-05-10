9e14cfe1b689c69a85afb39bbe5d4e85ff0c75e8
Damir <dense.neer@gmail.com>
2023-05-10 09:21:46 +0600

    create empty class MyHashTable

---
+++ src/MyHashTable.java
@@ -0,0 +1,6 @@
+public class MyHashTable <K, V> {
+    private class HashNode<K, V>{}
+    private HashNode<K, V>[] chainArray;
+    private int M = 11;// default number of chain
+    private int size;
+}
