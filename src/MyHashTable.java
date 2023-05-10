51b912fbedf6e72336e58c5eb28a2cde71bcf556
Damir <dense.neer@gmail.com>
2023-05-10 09:27:58 +0600

    create empty methods

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -3,4 +3,14 @@ public class MyHashTable <K, V> {
     private HashNode<K, V>[] chainArray;
     private int M = 11;// default number of chain
     private int size;
+
+    public MyHashTable(){}
+    public MyHashTable(int M){}
+
+    private int hash(K key){return 0;}
+    public void put(K key, V value){}
+    public V get(K key){return null;}
+    public V remove(K key){return null;}
+    public boolean contains(V value){return false;}
+    public K getKey(V value){return null;}
 }
